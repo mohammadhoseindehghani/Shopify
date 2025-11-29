cc7d34e392b8637f42e040b174e83c4b824bba13
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-11-29 19:26:41 +0330

    Add project files.

---
+++ src/Presentation/Shopify.Presentation.RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Shopify.Presentation.RazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
