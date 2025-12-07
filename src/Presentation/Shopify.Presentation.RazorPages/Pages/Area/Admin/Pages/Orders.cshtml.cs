2942af98d4cb08378c12ec97eff7d88f8199b7f9
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-12-07 17:46:38 +0330

    pass real date to order ui

--- src/Presentation/Shopify.Presentation.RazorPages/Pages/Area/Admin/Pages/Orders.cshtml.cs
+++ src/Presentation/Shopify.Presentation.RazorPages/Pages/Area/Admin/Pages/Orders.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shopify.Domain.Core.OrderAgg.AppService;
+using Shopify.Domain.Core.OrderAgg.Dto;
 
 namespace Shopify.Presentation.RazorPages.Pages.Area.Admin.Pages
 {
-    public class OrdersModel : PageModel
+    public class OrdersModel(IOrderAppService orderAppService) : PageModel
     {
-        public void OnGet()
+        public ICollection<OrderDto> Orders { get; set; }
+        public async Task OnGet(CancellationToken cancellationToken)
         {
+            Orders = await orderAppService.GetAll(cancellationToken);
         }
     }
 }
