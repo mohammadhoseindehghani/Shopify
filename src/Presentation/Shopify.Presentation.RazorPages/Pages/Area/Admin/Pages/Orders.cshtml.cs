7d5cc5e78b25855f44a3c4cfbe175b8eab63ac49
Mohammadhosein Dehghani <dehghani.fullstack@gmail.com>
2025-12-07 18:15:12 +0330

    fix show img of orderItem in modal

--- src/Presentation/Shopify.Presentation.RazorPages/Pages/Area/Admin/Pages/Orders.cshtml.cs
+++ src/Presentation/Shopify.Presentation.RazorPages/Pages/Area/Admin/Pages/Orders.cshtml.cs
@@ -8,9 +8,19 @@ namespace Shopify.Presentation.RazorPages.Pages.Area.Admin.Pages
     public class OrdersModel(IOrderAppService orderAppService) : PageModel
     {
         public ICollection<OrderDto> Orders { get; set; }
+        public ICollection<OrderItemDto> OrderItems { get; set; }
+
+
+
         public async Task OnGet(CancellationToken cancellationToken)
         {
             Orders = await orderAppService.GetAll(cancellationToken);
+        }
+
+        public async Task<IActionResult> OnGetOrderItems(int orderId, CancellationToken cancellationToken)
+        {
+            var items = await orderAppService.GetOrderItems(orderId, cancellationToken);
+            return new JsonResult(items);
         }
     }
 }
